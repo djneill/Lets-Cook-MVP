0e933a132af35444d095fabeda4391bcc68724cd
DJ Neill <dj.neill856@gmail.com>
2023-07-30 19:05:08 -0500

    fixed duplicate key error when adding comments

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  userName: { type: String, unique: true },
+  userName: { type: String, unique: true, required: true },
   email: { type: String, unique: true },
   password: String,
 });
